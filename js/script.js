4a84a12aa51a7aa5958d0d1264bca7a2d21096a3
Analubk <ana.breginski.ifpr.2023@gmail.com>
2025-11-26 19:31:15 -0300

    nao funciona

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-var salario = document.getElementById("salario").value.parseFloat
+var salario = document.getElementById("salario").value
 
 document.getElementById("formDespesas").addEventListener("submit", function(event){
-    var data = document.getElementById("data").value
+    event.preventDefault()
+
+    var data = document.getElementById("data").value.format('DD/MM/YYYY')
     var nome = document.getElementById("nome").value
-    var valor = document.getElementById("valor").value.parseFloat
+    var valor = document.getElementById("valor").value
 
     var despesa = {data:data, nome:nome, valor:valor}
 
@@ -13,10 +15,25 @@ document.getElementById("formDespesas").addEventListener("submit", function(even
     localStorage.setItem('listagem', JSON.stringify(historicoDespesas))
 
     document.getElementById("formDespesas").reset()
+    
     exibir_despesas()
+    atualizarSaldo()
+
 })
 
 function exibir_despesas(){
     var historicoDespesas = JSON.parse(localStorage.getItem('listagem')) || []
     var output = document.getElementById("output")
+    output.innerHTML = ""
+    for(let i=0; i<historicoDespesas.length; i++){
+        let li = document.createElement('li')
+        li.textContent = 'Data:' + historicoDespesas[i].data + ' Nome:' + historicoDespesas[i].nome + ' Valor:' + historicoDespesas[i].valor
+        output.appendChild(li)
+    }
+}
+
+function atualizarSaldo(){
+    var pSalario = document.getElementById("pSalario")
+    var pDespesas = document.getElementById("pDespesas")
+    var pSaldo = document.getElementById("pSaldo")
 }
